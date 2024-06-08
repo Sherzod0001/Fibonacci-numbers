abf8196161f27fc6361ef8cc6c7c643e9ff750e5
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-08 20:12:02 +0500

    update

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-
-Console.WriteLine("Welcome!!!! This program is a program that calculates the fibonacci sequence");
-Console.Write("Enter value: ");
-string userInput = Console.ReadLine();
-int fibonacciEnd = Convert.ToInt32(userInput);
-
-int fibonacciStartOne = 0;
-int fibonacciStartTwo = 1;
-
-while (fibonacciStartOne <= fibonacciEnd)
+while (true)
 {
-    Console.Write($"{fibonacciStartOne}, ");
-    int nextFibonacci = fibonacciStartOne + fibonacciStartTwo;
-    fibonacciStartOne = fibonacciStartTwo;
-    fibonacciStartTwo = nextFibonacci;
+    Console.WriteLine("Welcome!!!! This program is a program that calculates the fibonacci sequence");
+    Console.Write("Enter value: ");
+    string userInput = Console.ReadLine();
+    int fibonacciEnd = Convert.ToInt32(userInput);
+
+    int fibonacciStartOne = 0;
+    int fibonacciStartTwo = 1;
+
+    while (fibonacciStartOne <= fibonacciEnd)
+    {
+        Console.Write($"{fibonacciStartOne}, ");
+        int nextFibonacci = fibonacciStartOne + fibonacciStartTwo;
+        fibonacciStartOne = fibonacciStartTwo;
+        fibonacciStartTwo = nextFibonacci;
+        
+    }
+    Console.WriteLine();
+    Console.WriteLine("Do you want to use the program again? 👉 y/n 👈");
+    string answer = Console.ReadLine();
+
+    if (answer.ToLower() != "y")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Thank you for using the program 🤝");
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Fibonacci-numbers
+># Fibonacci-numbers
+![Fibonacci series](/images/Screenshot%202024-06-08%20at%207.40.19%20PM.png)
+
+

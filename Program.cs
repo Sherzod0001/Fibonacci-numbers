8724e94a9563353def3ee64b1d9f63c2e5914c63
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-08 19:37:47 +0500

    Initialize project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Welcome!!!! This program is a program that calculates the fibonacci sequence");
+Console.Write("Enter value: ");
+string userInput = Console.ReadLine();
+int fibonacciEnd = Convert.ToInt32(userInput);
+
+int fibonacciStartOne = 0;
+int fibonacciStartTwo = 1;
+
+while (fibonacciStartOne <= fibonacciEnd)
+{
+    Console.Write($"{fibonacciStartOne}, ");
+    int nextFibonacci = fibonacciStartOne + fibonacciStartTwo;
+    fibonacciStartOne = fibonacciStartTwo;
+    fibonacciStartTwo = nextFibonacci;
+}
